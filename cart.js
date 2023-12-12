02616e87b8dadf828b948b2d072958da0d0249ad
Sahil Shile <sahilshile25@gmail.com>
2023-12-13 01:00:28 +0530

    Format for any website

---
+++ cart.js
@@ -0,0 +1,23 @@
+
+let cart = [];
+let total = 0;
+
+function addToCart(productName, price) {
+  cart.push({ name: productName, price: price });
+  total += price;
+  updateCart();
+}
+
+function updateCart() {
+  const cartItems = document.getElementById('cart-items');
+  const cartTotal = document.getElementById('cart-total');
+  
+  cartItems.innerHTML = '';
+  cart.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = `${item.name} - $${item.price}`;
+    cartItems.appendChild(li);
+  });
+
+  cartTotal.textContent = total.toFixed(2);
+}
